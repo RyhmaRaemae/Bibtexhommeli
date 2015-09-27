fc61085c5dc3e51f5db5f7ba190129720f64033b
Jacinto Ramirez Lahti <jacinto.ramirez.lahti@cs.helsinki.fi>
2015-09-27 15:19:11 +0300

    Added dummy unit test to test Travis-CI

---
+++ BibtexApp/src/test/java/BookTest.java
@@ -0,0 +1,35 @@
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class BookTest {
+    
+    public BookTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // Dummy test to test TravisCI
+    @Test
+    public void dummy() {
+        assertEquals(0, 0);
+    }
+}
