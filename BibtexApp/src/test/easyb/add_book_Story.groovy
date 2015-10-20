e96b5cdd70c116f5beb2e6670be8c6e1320cf8f8
Jere Hupanen <jere.hupanen@cs.helsinki.fi>
2015-10-21 00:20:05 +0300

    copy paste ja tyhjät metodit poistettu

--- BibtexApp/src/test/easyb/add_book_Story.groovy
+++ BibtexApp/src/test/easyb/add_book_Story.groovy
@@ -6,50 +6,6 @@ import java.util.*
 
 description 'User can add books into the app'
 
-scenario "user can login with correct password", {
-  given 'command addbook selected', {
-  }
-
-  when 'the required fields are filled', {
-  }
-
-  then 'the book will be saved', {
-  }
-}
-
-scenario "user cannot add a book without the author field", {
-  given 'command addbook selected', {
-  }
-
-  when 'the Author field is left empty', {
-  }
-
-  then 'the book will not be saved', {
-  }
-}
-
-scenario "user cannot add a book without the title field", {
-  given 'command addbook selected', {
-  }
-
-  when 'the title field is left empty', {
-  }
-
-  then 'the book will not be saved', {
-  }
-}
-
-scenario "user cannot add a book without the year field", {
-  given 'command addbook selected', {
-  }
-
-  when 'the title year is left empty', {
-  }
-
-  then 'the book will not be saved', {
-  }
-}
-
 scenario "adding author, year and title fields to a new book", {
 
     given "an empty empty book",{
--- BibtexApp/src/test/easyb/save_references_to_file_Story.groovy
+++ BibtexApp/src/test/easyb/save_references_to_file_Story.groovy
@@ -26,24 +26,3 @@ scenario "a BibTex.bib file is generated and bibtex is added into it", {
         storage.loadFromFile("BibTeX.bib").shouldNotBe(null)
     }
 }
-
-scenario "a BibTex.bib file is generated and bibtex is added into it", {
-    given 'a new book reference is created', {
-        storage = new ReferenceStorage(new ArrayList<Reference>());
-        io = new StubIO("add", "1", "Arto Vihavainen", "Ohjelmistotuotanto", "2015", "", "", "", "", "", "", "save", "quit")
-        addRef = new AddReference(io, storage)
-        listRef = new ListReferences(io, storage)
-        l = new ArrayList<TextUIFunction>()
-        l.add(addRef)
-        l.add(listRef)
-        ui = new TextUI(l, io)
-    }
-
-    when 'the book is entered into the system and it is saved into a bibtex file', {
-        ui.run()
-    }
-
-    then 'the file exists', {            
-        storage.loadFromFile("BibTeX.bib").shouldNotBe(null)
-    }
-}
