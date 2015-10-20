8ac985ee29abf75946d999fc84e836ae98f67d9a
Jere Hupanen <jere.hupanen@cs.helsinki.fi>
2015-10-21 00:20:46 +0300

    Testi kokeiluja

---
+++ BibtexApp/src/test/easyb/load_references_from_file_Story.groovy
@@ -0,0 +1,30 @@
+import raemae.bibtexapp.*
+import raemae.bibtexapp.domain.*
+import raemae.bibtexapp.ui.*
+import raemae.bibtexapp.services.*
+import java.util.*
+
+description "References are loaded from a file"
+
+scenario "a BibTex.bib file exists  or is generated", {
+    given 'a new book reference is created', {
+        storage = new ReferenceStorage(new ArrayList<Reference>());
+        io = new StubIO("add", "1", "aaaa", "aaaa", "aaaa", "", "", "", "", "", "", "save", "add", "1", "bbbb", "bbbb", "bbbb", "", "", "", "", "", "", "load", "list", "1", "quit")
+        addRef = new AddReference(io, storage)
+        listRef = new ListReferences(io, storage)
+        l = new ArrayList<TextUIFunction>()
+        l.add(addRef)
+        l.add(listRef)
+        ui = new TextUI(l, io)
+    }
+
+    when 'file reads content fom loaded file', {
+        ui.run()
+    }
+
+    then 'the program outputs contents from loaded file, instead of previous entry', {            
+        prints = io.getPrints()
+        prints.shouldHave("title = {aaaa}")
+        prints.shouldNotHave("title = {bbbb}")
+    }
+}
---
+++ BibtexApp/src/test/java/raemae/bibtexapp/services/LoadReferencesFromFileTest.java
@@ -0,0 +1,72 @@
+package raemae.bibtexapp.services;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import org.junit.After;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import static org.junit.internal.matchers.StringContains.containsString;
+import raemae.bibtexapp.domain.*;
+
+public class LoadReferencesFromFileTest {
+
+    private ReferenceStorage references = new ReferenceStorage(new ArrayList<Reference>());
+    private ReferenceStorage references2 = new ReferenceStorage(new ArrayList<Reference>());
+
+    private String convertedPath = "BibTeXTest.bib";
+
+    @Before
+    public void setUp() {
+        File file = new File(convertedPath);
+        if (file.exists()) {
+            try {
+                Files.delete(file.toPath());
+            } catch (IOException x) {
+                System.err.println(x);
+            }
+        }
+    }
+    
+    @After
+    public void tearDown() {
+        File file = new File(convertedPath);
+        if (file.exists()) {
+            try {
+                Files.delete(file.toPath());
+            } catch (IOException x) {
+                System.err.println(x);
+            }
+        }
+    }
+
+    @Test
+    public void fileIsLoadedSuccessfully() throws FileNotFoundException, IOException { 
+        Book book = new Book();
+        book.addField("author", "Vihavainen, Arto");
+        book.addField("year", "2015");
+        book.addField("title", "Ohjelmistotuotanto");        
+        references.addReference(book);
+        references.saveToFile(convertedPath);
+        
+        Book book2 = new Book();
+        book.addField("author", "bbbb");
+        book.addField("year", "bbbb");
+        book.addField("title", "bbbb");        
+        references.addReference(book);
+        references2.loadFromFile(convertedPath);
+        
+        File file = new File(convertedPath);
+        assertFalse(references2.getReferences() == references);
+
+    }
+
+}
