e39e4e9cbe1793575a692600f0f01955fa886736
banjohirvi <ollilari@gmail.com>
2015-10-21 22:13:40 +0300

    Deleted unused methods

--- BibtexApp/src/main/java/raemae/bibtexapp/services/ReferenceFilter.java
+++ BibtexApp/src/main/java/raemae/bibtexapp/services/ReferenceFilter.java
@@ -9,31 +9,6 @@ import raemae.bibtexapp.services.matchers.*;
 
 public class ReferenceFilter {
     
-    /*
-    public static Reference findByCitationKey(String citationKey, ReferenceStorage references) {
-        for (Reference r : references.getReferences()) {
-           if (r.getCitationKey().equals(citationKey)) {
-               return r;
-           }
-        }
-        return null;
-    }
-    
-    
-    public static List<Reference> findByType(String type, ReferenceStorage references) {
-        List<Reference> matches = findByMatcher(new TypeIs(type), references);
-        return matches;
-    }
-    
-    public static List<Reference> findByAnyFieldContains(String value, ReferenceStorage references) {
-        return findByMatcher(new AnyFieldContainsValue(value), references);
-    }
-    
-    public static List<Reference> findbyFieldContains(String field, String value, ReferenceStorage references) {
-        List<Reference> matches = findByMatcher(new FieldContainsValue(field, value), references);
-        return matches;
-    }*/
-    
     public static List<Reference> findByMatcher(Matcher m, ReferenceStorage references) {        
         List<Reference> matches = new ArrayList<Reference>();
         for (Reference r : references.getReferences()) {
